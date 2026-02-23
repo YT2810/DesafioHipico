55eded930790c529a7ce597ca1a6eb3208644d08
yolfry <yolfry@gmail.com>
2026-02-22 21:53:36 -0300

    feat: add Google Analytics (G-DB6H4TPMJ1) via next/script in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProviderWrapper from "@/components/SessionProviderWrapper";
 import WhatsAppButton from "@/components/WhatsAppButton";
+import Script from "next/script";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID ?? 'G-DB6H4TPMJ1';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +30,18 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950 text-gray-100`}>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
         <SessionProviderWrapper>
           {children}
           <WhatsAppButton />
